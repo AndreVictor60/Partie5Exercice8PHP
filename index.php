0935dae8ad0a54f863b3784afa7cf509185b564b
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:33:29 +0100

    fini

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 5 Exercice 8</title>
+  </head>
+  <body>
+    <?php
+      $month=['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+      foreach ($month as $value) {
+        echo $value.'<br />';
+      }
+     ?>
+  </body>
+</html>
